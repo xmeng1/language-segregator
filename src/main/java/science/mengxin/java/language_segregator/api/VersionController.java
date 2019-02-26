61ea9adb1247a39c002514ba7021bf043cbf8095
Xin Meng <x.meng@outlook.com>
2019-02-26 08:07:16 +0000

    simple version restful api

---
+++ src/main/java/science/mengxin/java/language_segregator/api/VersionController.java
@@ -0,0 +1,21 @@
+package science.mengxin.java.language_segregator.api;
+
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RequestParam;
+        import org.springframework.web.bind.annotation.ResponseBody;
+        import science.mengxin.java.language_segregator.model.VersionInfo;
+
+        import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
+public class VersionController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/version")
+    @ResponseBody
+    public VersionInfo getVersion(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
+        return new VersionInfo("0.0.1");
+    }
+}
---
+++ src/main/java/science/mengxin/java/language_segregator/model/VersionInfo.java
@@ -0,0 +1,17 @@
+package science.mengxin.java.language_segregator.model;
+
+public class VersionInfo {
+    String number;
+
+    public VersionInfo(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
