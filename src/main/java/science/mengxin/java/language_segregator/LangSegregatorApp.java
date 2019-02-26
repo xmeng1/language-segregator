67df0599db1613597eb2f95d737c769841f7982e
Xin Meng <x.meng@outlook.com>
2019-02-26 08:09:05 +0000

    shorter the app class name

--- src/main/java/science/mengxin/java/language_segregator/LangSegregatorApp.java
+++ src/main/java/science/mengxin/java/language_segregator/LangSegregatorApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LanguageSegregatorApplication {
+public class LangSegregatorApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(LanguageSegregatorApplication.class, args);
+        SpringApplication.run(LangSegregatorApp.class, args);
     }
 
 }
