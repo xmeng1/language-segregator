5842a248975e4a39f8871ad61f35ba48592fed56
Xin Meng <x.meng@outlook.com>
2019-02-26 07:49:30 +0000

    initial

---
+++ src/main/java/science/mengxin/java/language_segregator/LanguageSegregatorApplication.java
@@ -0,0 +1,13 @@
+package science.mengxin.java.language_segregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageSegregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanguageSegregatorApplication.class, args);
+    }
+
+}
