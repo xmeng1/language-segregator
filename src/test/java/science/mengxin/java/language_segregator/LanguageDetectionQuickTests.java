75c3171567f825962bd0c7a0c4754c6dc0520952
Xin Meng <x.meng@outlook.com>
2019-02-27 21:34:06 +0000

    test mix string

--- src/test/java/science/mengxin/java/language_segregator/LanguageDetectionQuickTests.java
+++ src/test/java/science/mengxin/java/language_segregator/LanguageDetectionQuickTests.java
@@ -26,7 +26,10 @@ import java.util.List;
 public class LanguageDetectionQuickTests {
 
     String persianStr = "طراحی ساده اولد طوری کار می کند که شما می\u200Cتونید این را که محرک نامیده میشه پشتش صفحه نمایش نصب کنید. این ها چیزای بسیار بسیار نازکیه که کل صفحه نمایش را به یک بلندگو تبدیل می کنه. پشت سر این صفحه نمایش به نظرم پنج محرک نصب شده و طوری کار می کنه که به نظر میاد صدا واقعا از جای مشخص صفحه بیرون میآد و به این دلیله که در واقع همینطور هم است.";
-
+    String mixStr = "This is a bag 两国领导人在国际媒体前握手并拍照，有说有笑，随后接受记者访问。金正恩表示，确信\"金特会\"会取得所有人都满意的结果，他说：“我们克服了所有阻碍，今日来到了这里。”美国总统特朗普则表示，他期待这次峰会是一次“非常成功”的峰会。他说：“能和金委员长在一起是一种荣誉。”";
+    String mixStr5 = "两国领导人在国际媒体前握手并拍照，有说有笑，随后接受记者访问。金正恩表示，确信\"金特会\"会取得所有人都满意的结果，他说：“我们克服了所有阻碍，今日来到了这里。”美国总统特朗普则表示，他期待这次峰会是一次“非常成功”的峰会。他说：“能和金委员长在一起是一种荣誉。”";
+    String mixStr3 = "但是我们不能使用它，This is a bag,  why?";
+    String mixStr2 = "This is a bag, 但是我们不能使用它，why?";
 
     /**
      * https://github.com/optimaize/language-detector
@@ -61,6 +64,34 @@ public class LanguageDetectionQuickTests {
         }
     }
 
+    @Test
+    public void languageDetectMixTest() throws IOException {
+        //load all languages:
+        List<LanguageProfile> languageProfiles = new LanguageProfileReader().readAllBuiltIn();
+
+        //build language detector:
+        LanguageDetector languageDetector = LanguageDetectorBuilder.create(NgramExtractors.standard())
+                .withProfiles(languageProfiles)
+                .minimalConfidence(0.99d)
+                .build();
+
+//create a text object factory
+        TextObjectFactory textObjectFactory = CommonTextObjectFactories.forDetectingOnLargeText();
+//        TextObjectFactory textObjectFactory = CommonTextObjectFactories.forDetectingShortCleanText();
+
+//query:
+//        TextObject textObject = textObjectFactory.forText("this is my bag");
+        TextObject textObject = textObjectFactory.forText(mixStr);
+        // NOTE: if you want to select this RTL string, need only there RTL string not include others.
+
+        Optional<LdLocale> lang = languageDetector.detect(textObject);
+        if (lang.isPresent()) {
+            System.out.println(lang.get().toString());
+        }else {
+            System.out.println("cannot get the result");
+        }
+    }
+
     /**
      * https://detectlanguage.com/
      * https://github.com/detectlanguage/detectlanguage-java
@@ -81,6 +112,20 @@ public class LanguageDetectionQuickTests {
         System.out.println("Confidence: " + result.confidence);
     }
 
+    @Test
+    public void languageDetectApiMixTest() throws IOException, APIError {
+        DetectLanguage.apiKey = "91ff5462776798ad617da86fdc6131aa";
+
+        // Enable secure mode (SSL) if passing sensitive information
+        // DetectLanguage.ssl = true;
+        List<Result> results = DetectLanguage.detect(mixStr);
+
+        Result result = results.get(0);
+
+        System.out.println("Language: " + result.language);
+        System.out.println("Is reliable: " + result.isReliable);
+        System.out.println("Confidence: " + result.confidence);
+    }
 
     @Test
     public void languageDetectShuyoTest() throws LangDetectException {
@@ -88,6 +133,18 @@ public class LanguageDetectionQuickTests {
 
         Detector detector = DetectorFactory.create();
         detector.append(persianStr);
+
+        String lang = detector.detect();
+
+        System.out.println(lang);
+    }
+
+    @Test
+    public void languageDetectMixShuyoTest() throws LangDetectException {
+        DetectorFactory.loadProfile(LanguageDetectionQuickTests.class.getResource("/profiles").getPath());
+
+        Detector detector = DetectorFactory.create();
+        detector.append(mixStr);
 
         String lang = detector.detect();
 
