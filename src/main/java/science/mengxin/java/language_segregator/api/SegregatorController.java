9567cdac459ede5ebe56743b7b5d5f1d3be524ae
Xin Meng <x.meng@outlook.com>
2019-03-02 08:25:24 +0000

    add initial test post api

--- src/main/java/science/mengxin/java/language_segregator/api/SegregatorController.java
+++ src/main/java/science/mengxin/java/language_segregator/api/SegregatorController.java
@@ -1,12 +1,17 @@
 package science.mengxin.java.language_segregator.api;
 
+import ch.qos.logback.core.util.ContentTypeUtil;
+import com.sun.xml.internal.ws.api.pipe.ContentType;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import science.mengxin.java.language_segregator.model.SegItem;
 import science.mengxin.java.language_segregator.model.SegRequest;
 import science.mengxin.java.language_segregator.model.basic.Result;
@@ -14,7 +19,7 @@ import science.mengxin.java.language_segregator.model.VersionInfo;
 import science.mengxin.java.language_segregator.model.basic.ResultList;
 import science.mengxin.java.language_segregator.service.SegregatorService;
 
-@Controller
+@RestController
 @V1APIController
 public class SegregatorController {
 
@@ -27,9 +32,11 @@ public class SegregatorController {
     this.segregatorService = segregatorService;
   }
 
-  @PostMapping("/split")
+  @PostMapping(value = "/split", produces= MediaType.APPLICATION_JSON_UTF8_VALUE)
   @ResponseBody
-  public ResultList<SegItem> getVersion(@RequestBody SegRequest segRequest) {
+  public ResultList<SegItem> splitString(@RequestBody SegRequest segRequest) {
+//    return new VersionInfo("0.0.1");
     return segregatorService.split(segRequest);
   }
+
 }
--- src/main/java/science/mengxin/java/language_segregator/service/SegregatorServiceImpl.java
+++ src/main/java/science/mengxin/java/language_segregator/service/SegregatorServiceImpl.java
@@ -1,5 +1,7 @@
 package science.mengxin.java.language_segregator.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,21 @@ public class SegregatorServiceImpl implements SegregatorService {
   @Override
   public ResultList<SegItem> split(SegRequest segRequest) {
     logger.debug("start SegregatorService split {}", segRequest.toString());
-    return null;
+
+    // get the string
+
+    // separate the string to fragment
+
+    // detect every fragment
+
+    // build result
+
+
+    ResultList<SegItem> resultList = new ResultList<>();
+    List<SegItem> segItemList = new ArrayList<>();
+    segItemList.add(new SegItem());
+    segItemList.add(new SegItem());
+    resultList.setList(segItemList);
+    return resultList;
   }
 }
