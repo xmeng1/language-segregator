e68acdfe0ca933837d9dde4123fcf9eabc4b339f
Xin Meng <x.meng@outlook.com>
2019-03-02 11:50:49 +0000

    clean version old test

--- src/main/java/science/mengxin/java/language_segregator/api/VersionController.java
+++ src/main/java/science/mengxin/java/language_segregator/api/VersionController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import science.mengxin.java.language_segregator.TheVersionClass;
-import science.mengxin.java.language_segregator.model.VersionInfo;
 
 import java.util.concurrent.atomic.AtomicLong;
 import science.mengxin.java.language_segregator.model.basic.Result;
@@ -15,9 +13,6 @@ import science.mengxin.java.language_segregator.utilities.GitVersionUtils;
 @Controller
 @V1APIController
 public class VersionController {
-
-  private static final String template = "Hello, %s!";
-  private final AtomicLong counter = new AtomicLong();
 
   @GetMapping("/version")
   @ResponseBody
