9639a0a1bf4a6387bdce41c92ac0b0e2f0af661f
Xin Meng <x.meng@outlook.com>
2019-03-03 20:23:52 +0000

    add plugin to build and copy dist to target

---
+++ src/main/java/science/mengxin/java/language_segregator/api/HomeController.java
@@ -0,0 +1,14 @@
+package science.mengxin.java.language_segregator.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+  @GetMapping
+  public String home() {
+    return "forward:/index.html";
+  }
+}
